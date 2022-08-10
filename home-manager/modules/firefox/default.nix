2ee2aba8bc9a185dc5c01731b79afd40ff8b69eb
ivan770 <ivan@ivan770.me>
2022-08-10 16:31:45 +0300

    Make VDPAU optional for Nvidia, Firefox HW acceleration

--- home-manager/modules/firefox/default.nix
+++ home-manager/modules/firefox/default.nix
@@ -12,10 +12,16 @@ with lib; {
   config = mkMerge [
     (mkIf cfg.enable {
       home.packages = [ pkgs.firefox ];
+      systemd.user.sessionVariables = {
+        # https://github.com/elFarto/nvidia-vaapi-driver/#firefox
+        MOZ_DISABLE_RDD_SANDBOX = 1;
+      };
     })
     (mkIf cfg.wayland {
       systemd.user.sessionVariables = {
         MOZ_ENABLE_WAYLAND = 1;
+        # https://github.com/elFarto/nvidia-vaapi-driver/#firefox
+        EGL_PLATFORM = "wayland";
       };
     })
   ];

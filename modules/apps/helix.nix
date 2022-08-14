9529bd0935efba853e3135fb171ecfe772cb95bf
ivan770 <ivan@ivan770.me>
2022-08-14 17:03:16 +0300

    Helix, Discord config

---
+++ modules/apps/helix.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.essentia.programs.helix;
+in
+with lib; {
+  options.essentia.programs.helix = {
+    settings = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "Helix settings file contents";
+    };
+  };
+
+  config.programs.helix = mkMerge [
+    {
+      enable = true;
+    }
+    (mkIf (isString cfg.settings) {
+      settings = builtins.fromTOML cfg.settings;
+    })
+  ];
+}

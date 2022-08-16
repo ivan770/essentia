13c18615423f2321373d563efff927d9191f8f00
ivan770 <ivan@ivan770.me>
2022-08-16 19:58:00 +0300

    System-wide locale module

---
+++ modules/common/locale.nix
@@ -0,0 +1,62 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.essentia.locale;
+in
+  with lib; {
+    options.essentia.locale = {
+      base = mkOption {
+        type = types.str;
+        description = "Primary language that is used to display system interface";
+      };
+
+      units = mkOption {
+        type = types.str;
+        description = "Language used to display units";
+      };
+
+      timeZone = mkOption {
+        type = types.str;
+        description = "Global time zone configuration";
+      };
+
+      supportedLocales = mkOption {
+        type = types.listOf types.str;
+        description = "Locales for glibc to support";
+        default = [
+          "en_US.UTF-8/UTF-8"
+          "uk_UA.UTF-8/UTF-8"
+          "ru_UA.UTF-8/UTF-8"
+        ];
+      };
+    };
+
+    config = {
+      console = {
+        font = "Lat2-Terminus16";
+        keyMap = "us";
+      };
+      i18n = {
+        defaultLocale = cfg.base;
+        extraLocaleSettings = {
+          LC_CTYPE = cfg.base;
+          LC_COLLATE = cfg.base;
+          LC_NUMERIC = cfg.base;
+          LC_MESSAGES = cfg.base;
+
+          LC_ADDRESS = cfg.units;
+          LC_MEASUREMENT = cfg.units;
+          LC_MONETARY = cfg.units;
+          LC_NAME = cfg.units;
+          LC_PAPER = cfg.units;
+          LC_TELEPHONE = cfg.units;
+          LC_TIME = cfg.units;
+        };
+        supportedLocales = cfg.supportedLocales;
+      };
+      time.timeZone = cfg.timeZone;
+    };
+  }
--- modules/desktop/generic.nix
+++ modules/desktop/generic.nix
@@ -1,33 +1,10 @@
-{
-  lib,
-  config,
-  nixosModules,
-  ...
-}: let
-  cfg = config.essentia.desktop;
-in
-  with lib; {
-    options.essentia.desktop = {
-      wayland = mkEnableOption "Enable Wayland support";
-
-      profiles = mkOption {
-        type = types.attrsOf types.str;
-        default = {};
-        description = "Users and their corresponding profiles.";
-      };
-    };
-
-    imports = with nixosModules; [
-      common.home-manager
-      common.global-locale
-      common.nix
-      desktop.fonts
-      desktop.gnome
-      desktop.plymouth
-    ];
-
-    config.essentia = {
-      home-manager.profiles = cfg.profiles;
-      gnome.wayland = cfg.wayland;
-    };
-  }
+{nixosModules, ...}: {
+  imports = with nixosModules; [
+    common.home-manager
+    common.locale
+    common.nix
+    desktop.fonts
+    desktop.gnome
+    desktop.plymouth
+  ];
+}

bdd1876dc8431c61d6608485172f5b24e097c19a
ivan770 <ivan@ivan770.me>
2022-08-17 20:28:47 +0300

    Prettify gnome-terminal config

--- modules/profiles/ivan770/battlestation.nix
+++ modules/profiles/ivan770/battlestation.nix
@@ -37,33 +37,7 @@
     essentia.programs = {
       discord.settings = builtins.readFile ./discord/settings.json;
       firefox = import ./firefox/config.nix {inherit nur;};
-      gnome-terminal.settings = {
-        audibleBell = false;
-        colors = {
-          backgroundColor = "rgb(0,0,0)";
-          foregroundColor = "rgb(255,255,255)";
-          palette = [
-            "rgb(0,0,0)"
-            "rgb(205,0,0)"
-            "rgb(0,205,0)"
-            "rgb(205,205,0)"
-            "rgb(0,0,238)"
-            "rgb(205,0,205)"
-            "rgb(0,205,205)"
-            "rgb(229,229,229)"
-            "rgb(127,127,127)"
-            "rgb(255,0,0)"
-            "rgb(0,255,0)"
-            "rgb(255,255,0)"
-            "rgb(92,92,255)"
-            "rgb(255,0,255)"
-            "rgb(0,255,255)"
-            "rgb(255,255,255)"
-          ];
-        };
-        font = "JetBrains Mono 11";
-        loginShell = true;
-      };
+      gnome-terminal.settings = import ./gnome-terminal/config.nix {};
       gpg.sshKeys = [
         "B0E258EAD4123779C4CFA077DBD8328FD08BADF5"
       ];

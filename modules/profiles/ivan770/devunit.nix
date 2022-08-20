7de472b66bdc952f9f95333474b435edd7bd4833
ivan770 <ivan@ivan770.me>
2022-08-20 09:22:10 +0300

    Move HM Sway into a separate module

--- modules/profiles/ivan770/devunit.nix
+++ modules/profiles/ivan770/devunit.nix
@@ -11,6 +11,7 @@
     apps.editors.vscode
     apps.social.firefox
     apps.utilities.gpg
+    desktop.hm-sway
   ];
 
   config = {
@@ -19,30 +20,34 @@
         git
         tdesktop
         rnix-lsp
-        swaylock
-        wl-clipboard
-        alacritty
       ];
       stateVersion = "22.05";
     };
-    essentia.programs = {
-      firefox =
-        import ./firefox/config.nix {
-          inherit lib nur;
-          enableGnomeShell = false;
-        }
-        // {
-          wayland = true;
-        };
-      gpg.sshKeys = [
-        "B0E258EAD4123779C4CFA077DBD8328FD08BADF5"
-      ];
-      helix.settings = builtins.readFile ./helix/config.toml;
-      vscode =
-        import ./vscode/config.nix {inherit pkgs;}
-        // {
-          wayland = true;
-        };
+    essentia = {
+      programs = {
+        firefox =
+          import ./firefox/config.nix {
+            inherit lib nur;
+            enableGnomeShell = false;
+          }
+          // {
+            wayland = true;
+          };
+        gpg.sshKeys = [
+          "B0E258EAD4123779C4CFA077DBD8328FD08BADF5"
+        ];
+        helix.settings = builtins.readFile ./helix/config.toml;
+        vscode =
+          import ./vscode/config.nix {inherit pkgs;}
+          // {
+            wayland = true;
+          };
+      };
+      sway = {
+        swaySettings = import ./sway/sway.nix {inherit pkgs;};
+        waybarSettings = import ./sway/waybar/bars.nix {};
+        waybarStyle = builtins.readFile ./sway/waybar/style.css;
+      };
     };
     programs = {
       bash = {
@@ -50,43 +55,6 @@
         enableVteIntegration = true;
       };
       home-manager.enable = true;
-      waybar = {
-        enable = true;
-        settings = import ./sway/waybar/bars.nix {};
-        style = builtins.readFile ./sway/waybar/style.css;
-      };
-    };
-    wayland.windowManager.sway = {
-      enable = true;
-      wrapperFeatures = {
-        base = true;
-        gtk = true;
-      };
-      extraSessionCommands = ''
-        export SDL_VIDEODRIVER=wayland
-        export QT_QPA_PLATFORM=wayland
-        export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-      '';
-      config = {
-        terminal = "alacritty";
-        bars = [
-          {
-            command = "${pkgs.waybar}/bin/waybar";
-            position = "top";
-          }
-        ];
-        input."*" = {
-          xkb_layout = "us,ru,ua";
-          xkb_options = "grp:lalt_lshift_toggle";
-        };
-        input."type:touchpad" = {
-          tap = "enabled";
-          natural_scroll = "enabled";
-          scroll_method = "two_finger";
-        };
-        bindkeysToCode = true;
-        modifier = "Mod4";
-      };
     };
   };
 }

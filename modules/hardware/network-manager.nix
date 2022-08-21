291b1c9b9f8a8c95b91769381372c60364ed6c5c
ivan770 <ivan@ivan770.me>
2022-08-21 09:11:38 +0300

    systemd-resolved support

--- modules/hardware/network-manager.nix
+++ modules/hardware/network-manager.nix
@@ -1,6 +1,10 @@
 {config, ...}: {
-  config.networking = {
-    networkmanager.enable = true;
-    useDHCP = false;
+  config = {
+    networking = {
+      networkmanager.enable = true;
+      resolvconf.enable = false;
+      useDHCP = false;
+    };
+    services.resolved.enable = true;
   };
 }

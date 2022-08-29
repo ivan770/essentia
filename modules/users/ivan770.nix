d141edda8ea19a2f3635ac183c4a762d9a9c553e
ivan770 <ivan@ivan770.me>
2022-08-29 16:43:52 +0300

    Remove unused code in user declaration

--- modules/users/ivan770.nix
+++ modules/users/ivan770.nix
@@ -7,14 +7,12 @@
 with lib; {
   config.users.users.ivan770 = {
     isNormalUser = true;
-    home = "/home/ivan770";
     passwordFile = config.sops.secrets."users/ivan770/password".path;
     extraGroups =
       [
         "video"
         "wheel"
       ]
-      ++ optionals config.networking.networkmanager.enable ["networkmanager"]
       ++ optionals config.security.tpm2.enable [config.security.tpm2.tssGroup]
       ++ optionals config.hardware.sane.enable ["scanner" "lp"];
     openssh.authorizedKeys.keys = [

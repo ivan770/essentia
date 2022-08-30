72b85ce67f5d0b45bb99f6c806c79ee0259a204b
ivan770 <ivan@ivan770.me>
2022-08-30 14:57:45 +0300

    Also add common-pc to iridium

--- hosts/x86_64-linux/iridium/default.nix
+++ hosts/x86_64-linux/iridium/default.nix
@@ -9,6 +9,7 @@
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
     inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-pc
     inputs.nixos-hardware.nixosModules.common-pc-laptop
     inputs.nixos-hardware.nixosModules.common-pc-laptop-acpi_call
     inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd

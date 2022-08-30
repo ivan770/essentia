5d6856a5b23501aae382df825abf35f2fa6ae66c
ivan770 <ivan@ivan770.me>
2022-08-30 13:37:51 +0300

    Keep NIX_PATH in sync with flake inputs

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -10,11 +10,18 @@
       extraOptions = ''
         experimental-features = nix-command flakes
       '';
+
       settings.trusted-users = [
         "root"
         "@wheel"
       ];
+
       registry.nixpkgs.flake = inputs.nixpkgs;
+
+      nixPath = [
+        "nixpkgs=${inputs.nixpkgs}"
+        "/nix/var/nix/profiles/per-user/root/channels"
+      ];
     };
 
     nixpkgs.config.allowUnfree = true;

af882036d636018290f2c02246d937b37019bf93
ivan770 <ivan@ivan770.me>
2022-09-01 10:14:57 +0300

    Attempt to fix permissions on PostgreSQL secrets

--- modules/common/secrets.nix
+++ modules/common/secrets.nix
@@ -4,9 +4,18 @@
     secrets = {
       "users/ivan770/password".neededForUsers = true;
       networks = {};
-      "postgresql/ssl/server/cert" = {};
-      "postgresql/ssl/server/key" = {};
-      "postgresql/ssl/root" = {};
+      "postgresql/ssl/server/cert" = {
+        owner = "postgres";
+        group = "postgres";
+      };
+      "postgresql/ssl/server/key" = {
+        owner = "postgres";
+        group = "postgres";
+      };
+      "postgresql/ssl/root" = {
+        owner = "postgres";
+        group = "postgres";
+      };
     };
   };
 }

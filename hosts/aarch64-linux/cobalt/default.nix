c0b1162f60078e496e903a9ab0db55add985ac39
ivan770 <ivan@ivan770.me>
2022-09-01 10:49:28 +0300

    Add server-specific PostgreSQL config

--- hosts/aarch64-linux/cobalt/default.nix
+++ hosts/aarch64-linux/cobalt/default.nix
@@ -15,6 +15,25 @@
   config = {
     essentia = {
       networking.wired = true;
+      postgresql.serverConfig = {
+        # PGTune suggestions
+        max_connections = 25;
+        shared_buffers = "6GB";
+        effective_cache_size = "18GB";
+        maintenance_work_mem = "1536MB";
+        checkpoint_completion_target = 0.9;
+        wal_buffers = "16MB";
+        default_statistics_target = 100;
+        random_page_cost = 1.1;
+        effective_io_concurrency = 200;
+        work_mem = "125829kB";
+        min_wal_size = "1GB";
+        max_wal_size = "4GB";
+        max_worker_processes = 4;
+        max_parallel_workers_per_gather = 2;
+        max_parallel_workers = 4;
+        max_parallel_maintenance_workers = 2;
+      };
       systemd-boot.mountpoint = "/boot";
     };
 
--- modules/server/postgresql.nix
+++ modules/server/postgresql.nix
@@ -3,27 +3,40 @@
   lib,
   pkgs,
   ...
-}: {
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_14;
-    enableTCPIP = true;
-    settings = {
-      ssl = true;
-      ssl_cert_file = config.sops.secrets."postgresql/ssl/server/cert".path;
-      ssl_key_file = config.sops.secrets."postgresql/ssl/server/key".path;
-      ssl_ca_file = config.sops.secrets."postgresql/ssl/root".path;
+}: let
+  cfg = config.essentia.postgresql;
+in
+  with lib; {
+    options.essentia.postgresql = {
+      serverConfig = mkOption {
+        type = with types; attrsOf (oneOf [bool float int str]);
+        default = {};
+        description = "Server-specific PostgreSQL configuration.";
+      };
     };
-    authentication = ''
-      hostssl all all 0.0.0.0/0 cert clientcert=verify-full
-    '';
-    ensureUsers = [
-      {
-        name = "ivan770";
-        ensurePermissions = {
-          "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+
+    services.postgresql = {
+      enable = true;
+      package = pkgs.postgresql_14;
+      enableTCPIP = true;
+      settings =
+        cfg.serverConfig
+        // {
+          ssl = true;
+          ssl_cert_file = config.sops.secrets."postgresql/ssl/server/cert".path;
+          ssl_key_file = config.sops.secrets."postgresql/ssl/server/key".path;
+          ssl_ca_file = config.sops.secrets."postgresql/ssl/root".path;
         };
-      }
-    ];
-  };
-}
+      authentication = ''
+        hostssl all all 0.0.0.0/0 cert clientcert=verify-full
+      '';
+      ensureUsers = [
+        {
+          name = "ivan770";
+          ensurePermissions = {
+            "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+          };
+        }
+      ];
+    };
+  }

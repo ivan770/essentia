0533ba128e601cead975c3f4728288a882fe8871
ivan770 <ivan@ivan770.me>
2022-09-07 12:27:48 +0300

    Added acpid to correctly handle brightness keys

--- modules/hardware/backlight.nix
+++ modules/hardware/backlight.nix
@@ -1,3 +1,16 @@
-{...}: {
+{pkgs, ...}: {
   programs.light.enable = true;
+  services.acpid = {
+    enable = true;
+    handlers = {
+      brightnessUp = {
+        event = "video/brightnessup";
+        action = "${pkgs.light}/bin/light -A 5";
+      };
+      brightnessDown = {
+        event = "video/brightnessdown";
+        action = "${pkgs.light}/bin/light -U 5";
+      };
+    };
+  };
 }

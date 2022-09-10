d8bc54561e584bb7032ffb9cf73f2ce4742211be
ivan770 <ivan@ivan770.me>
2022-09-10 15:11:04 +0300

    Disable CPU microcode updates for cobalt

--- modules/hardware/firmware.nix
+++ modules/hardware/firmware.nix
@@ -8,14 +8,18 @@ in
   with lib; {
     options.essentia.firmware = {
       cpu.vendor = mkOption {
-        type = types.enum ["amd" "intel"];
+        type = types.nullOr (types.enum ["amd" "intel"]);
         description = "CPU vendor";
         example = "amd";
       };
     };
 
-    config.hardware = {
-      enableRedistributableFirmware = true;
-      cpu.${cfg.cpu.vendor}.updateMicrocode = true;
-    };
+    config.hardware = mkMerge [
+      {
+        enableRedistributableFirmware = true;
+      }
+      (mkIf (isString cfg.cpu.vendor) {
+        cpu.${cfg.cpu.vendor}.updateMicrocode = true;
+      })
+    ];
   }

706944ed7ce89e91b8596042a3f665f904180235
ivan770 <ivan@ivan770.me>
2022-09-11 10:27:36 +0300

    Add GBM_BACKEND environment variable to nvidia

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -2,6 +2,7 @@
   services.xserver.videoDrivers = ["nvidia"];
 
   environment.variables = {
+    GBM_BACKEND = "nvidia-drm";
     LIBVA_DRIVER_NAME = "nvidia";
   };
 
@@ -15,6 +16,8 @@
       enable = true;
       driSupport = true;
       driSupport32Bit = true;
+      # It seems that even though nixpkgs already contains installed VA-API shim, removing this section somehow breaks Firefox VA-API detection
+      # https://github.com/NixOS/nixpkgs/blob/74a1793c659d09d7cf738005308b1f86c90cb59b/nixos/modules/hardware/video/nvidia.nix#L317-L320
       extraPackages = with pkgs; [
         nvidia-vaapi-driver
       ];

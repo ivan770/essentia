2969633a2349e2f5e39192fb599988b1a0a84716
ivan770 <ivan@ivan770.me>
2022-09-13 11:44:12 +0300

    More Firefox configuration

--- modules/apps/social/firefox.nix
+++ modules/apps/social/firefox.nix
@@ -33,10 +33,52 @@ in
           settings =
             cfg.settings
             // {
-              # Privacy
+              # General privacy
+              "app.normandy.api_url" = "";
+              "app.normandy.enabled" = false;
               "app.shield.optoutstudies.enabled" = false;
+              "beacon.enabled" = false;
+              "breakpad.reportURL" = "";
+              "browser.crashReports.unsubmittedCheck.autoSubmit" = false;
+              "browser.crashReports.unsubmittedCheck.autoSubmit2" = false;
+              "browser.crashReports.unsubmittedCheck.enabled" = false;
               "browser.discovery.enabled" = false;
+              "browser.send_pings" = false;
+              "browser.tabs.crashReporting.sendReport" = false;
+              "datareporting.healthreport.service.enabled" = false;
               "datareporting.healthreport.uploadEnabled" = false;
+              "datareporting.policy.dataSubmissionEnabled" = false;
+              "experiments.enabled" = false;
+              "experiments.activeExperiment" = false;
+              "experiments.manifest.uri" = "";
+              "experiments.supported" = false;
+              "extensions.getAddons.cache.enabled" = false;
+              "extensions.getAddons.showPane" = false;
+              "extensions.shield-recipe-client.enabled" = false;
+              "extensions.shield-recipe-client.api_url" = "";
+              "network.allow-experiments" = false;
+              "services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSite" = false;
+              "toolkit.telemetry.enabled" = false;
+              "toolkit.telemetry.firstShutdownPing.enabled" = false;
+              "toolkit.telemetry.hybridContent.enabled" = false;
+              "toolkit.telemetry.newProfilePing.enabled" = false;
+              "toolkit.telemetry.prompted" = 2;
+              "toolkit.telemetry.rejected" = true;
+              "toolkit.telemetry.reportingpolicy.firstRun" = false;
+              "toolkit.telemetry.server" = "";
+              "toolkit.telemetry.shutdownPingSender.enabled" = false;
+              "toolkit.telemetry.unified" = false;
+              "toolkit.telemetry.unifiedIsOptIn" = false;
+              "toolkit.telemetry.updatePing.enabled" = false;
+
+              # Privacy by removal of device-specific APIs
+              "device.sensors.enabled" = false;
+              "device.sensors.ambientLight.enabled" = false;
+              "device.sensors.motion.enabled" = false;
+              "device.sensors.orientation.enabled" = false;
+              "device.sensors.proximity.enabled" = false;
+              "dom.battery.enabled" = false;
+              "dom.event.clipboardevents.enabled" = false;
 
               # Bloatware removal
               "extensions.pocket.enabled" = false;
@@ -59,6 +101,16 @@ in
               "services.sync.engine.addons" = false;
               "services.sync.engine.prefs" = false;
               "services.sync.engine.prefs.modified" = false;
+
+              # Disable auto-updates since those are managed by Nix anyway
+              "app.update.auto" = false;
+
+              # Configuration is expected to always be up-to-date, so remove Firefox suggestion
+              # to reset configuration
+              "browser.disableResetPrompt" = true;
+
+              # Disable bundled DoH since system is expected to have system-wide secure DNS already
+              "network.trr.mode" = 5;
 
               # https://github.com/elFarto/nvidia-vaapi-driver/#firefox
               "media.ffmpeg.vaapi.enabled" = true;
--- modules/profiles/ivan770/configs/firefox.nix
+++ modules/profiles/ivan770/configs/firefox.nix
@@ -20,6 +20,10 @@ with lib; {
     "browser.toolbars.bookmarks.visibility" = "never";
     "browser.newtabpage.activity-stream.feeds.topsites" = false;
     "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+    "browser.newtabpage.enhanced" = false;
+    "browser.newtabpage.introShown" = true;
+    "browser.startup.homepage_override.mstone" = "ignore";
+    "browser.urlbar.trimURLs" = false;
 
     # Disable microphone badge during calls
     "privacy.webrtc.legacyGlobalIndicator" = false;
@@ -31,18 +35,31 @@ with lib; {
     "font.name.serif.x-western" = "Noto Serif";
 
     # Privacy stuff
+    "browser.sessionstore.privacy_level" = 2;
     "dom.security.https_only_mode" = true;
     "dom.security.https_only_mode_ever_enabled" = true;
     "privacy.donottrackheader.enabled" = true;
+    "privacy.donottrackheader.value" = 1;
+    "privacy.query_stripping" = true;
     "privacy.trackingprotection.enabled" = true;
+    "privacy.trackingprotection.cryptomining.enabled" = true;
+    "privacy.trackingprotection.fingerprinting.enabled" = true;
+    "privacy.trackingprotection.pbmode.enabled" = true;
     "privacy.trackingprotection.socialtracking.enabled" = true;
 
+    # Disable noisy default browser check
+    "browser.shell.checkDefaultBrowser" = false;
+
     # Disable builtin password manager
+    "signon.autofillForms" = false;
     "signon.rememberSignons" = false;
     "signon.management.page.breach-alerts.enabled" = false;
 
     # Disable all notification prompts
     "permissions.default.desktop-notification" = 2;
+
+    # Disable about:config warning
+    "browser.aboutConfig.showWarning" = false;
 
     # Skip welcome screen
     "trailhead.firstrun.didSeeAboutWelcome" = true;

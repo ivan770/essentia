d608f0dd074839153e953ee4195196c95ae80fb0
ivan770 <ivan@ivan770.me>
2022-09-21 19:53:35 +0300

    Accept HTTPS connections from Cloudflare only

--- modules/server/firewall.nix
+++ modules/server/firewall.nix
@@ -1,14 +1,29 @@
-{...}: {
+{...}: let
+  mkCfIPList = version: sha256: (
+    builtins.replaceStrings ["\n"] [", "] (
+      builtins.readFile (builtins.fetchurl {
+        inherit sha256;
+        url = "https://www.cloudflare.com/ips-" + version;
+      })
+    )
+  );
+
+  cfV4List = mkCfIPList "v4" "0ywy9sg7spafi3gm9q5wb59lbiq0swvf0q3iazl0maq1pj1nsb7h";
+  cfV6List = mkCfIPList "v6" "1ad09hijignj6zlqvdjxv7rjj8567z357zfavv201b9vx3ikk7cy";
+in {
   networking.nftables = {
     enable = true;
     # Ruleset derived from https://wiki.nftables.org/wiki-nftables/index.php/Simple_ruleset_for_a_server
     ruleset = ''
+      define CF_IPV4 = { ${cfV4List} }
+      define CF_IPV6 = { ${cfV6List} }
+
       table inet firewall {
         chain input {
           type filter hook input priority 0; policy drop;
 
-          # Accept correct connections
-          ct state { established, related } accept
+          # Accept correct connections and immediately drop invalid ones
+          ct state vmap { established : accept, related : accept, invalid : drop }
 
           # Accept any loopback traffic
           iifname lo accept
@@ -17,8 +32,10 @@
           ip protocol icmp icmp type { destination-unreachable, router-advertisement, time-exceeded, parameter-problem } accept
           ip6 nexthdr icmpv6 icmpv6 type { destination-unreachable, packet-too-big, time-exceeded, parameter-problem, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } accept
 
-          # Allow only SSH and HTTPS connections
-          tcp dport { ssh, https } accept
+          # Allow SSH and HTTPS connections
+          tcp dport { https } ip saddr $CF_IPV4 accept
+          tcp dport { https } ip6 saddr $CF_IPV6 accept
+          tcp dport { ssh } accept
         }
 
         chain output {

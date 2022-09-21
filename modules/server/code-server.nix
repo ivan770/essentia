ddc8d24e4dc975463751cc2608912ebc2cf51873
ivan770 <ivan@ivan770.me>
2022-09-21 10:40:32 +0300

    Fix code-server group

--- modules/server/code-server.nix
+++ modules/server/code-server.nix
@@ -3,7 +3,7 @@
     enable = true;
     # FIXME: Hardcoded for now
     user = "ivan770";
-    group = "ivan770";
+    group = "users";
     # Authentication is expected to be provided by an upstream proxy server.
     auth = "none";
   };

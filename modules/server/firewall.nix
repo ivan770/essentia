93640eaf0a80fcd2f9d1fef29bd7e2134504c4e8
ivan770 <ivan@ivan770.me>
2022-09-24 12:11:01 +0300

    Added PostgreSQL to firewall config

--- modules/server/firewall.nix
+++ modules/server/firewall.nix
@@ -1,4 +1,4 @@
-{config, ...}: let
+{config, lib, ...}: let
   mkCfIPList = version: sha256: (
     builtins.replaceStrings ["\n"] [", "] (
       builtins.readFile (builtins.fetchurl {
@@ -10,6 +10,10 @@
 
   cfV4List = mkCfIPList "v4" "0ywy9sg7spafi3gm9q5wb59lbiq0swvf0q3iazl0maq1pj1nsb7h";
   cfV6List = mkCfIPList "v6" "1ad09hijignj6zlqvdjxv7rjj8567z357zfavv201b9vx3ikk7cy";
+
+  protectedPorts = [
+    (mkIf config.services.postgresql.enable config.services.postgresql.port)
+  ] ++ lib.optionals config.services.openssh.enable config.services.openssh.port;
 in {
   networking.nftables = {
     enable = true;
@@ -37,7 +41,7 @@ in {
           # Allow SSH and HTTPS connections
           tcp dport https ip saddr $CF_IPV4 accept
           tcp dport https ip6 saddr $CF_IPV6 accept
-          tcp dport ssh ip saddr $TRUSTED_NETWORKS accept
+          tcp dport { ${builtins.concatStringsSep ", " protectedPorts} } ip saddr $TRUSTED_NETWORKS accept
         }
 
         chain output {

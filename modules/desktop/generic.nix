dbd30bd45313ebf445dfbf962119f235afb93b91
ivan770 <ivan@ivan770.me>
2022-09-24 16:07:47 +0300

    Systemd in stage 1

--- modules/desktop/generic.nix
+++ modules/desktop/generic.nix
@@ -1,15 +1,6 @@
 {nixosModules, ...}: {
-  imports = with nixosModules; [
-    common.home-manager
-    common.locale
-    common.nix
-    common.secrets
-    common.security
-    common.ssh
-    desktop.console
-    desktop.fonts
-    desktop.kernel
-    desktop.plymouth
-    desktop.silent-boot
-  ];
+  imports = builtins.attrValues {
+    inherit (nixosModules.common) home-manager locale nix secrets security ssh systemd-initrd;
+    inherit (nixosModules.desktop) console fonts kernel plymouth silent-boot;
+  };
 }
--- modules/server/generic.nix
+++ modules/server/generic.nix
@@ -1,6 +1,6 @@
 {nixosModules, ...}: {
   imports = builtins.attrValues {
-    inherit (nixosModules.common) locale nix secrets security ssh;
+    inherit (nixosModules.common) locale nix secrets security ssh systemd-initrd;
     inherit (nixosModules.server) firewall kernel;
   };
 }

115fe0afefbdc4c1561b2dff4ae37879b73bdec5
ivan770 <ivan@ivan770.me>
2022-10-08 15:37:18 +0300

    Enable smart borders in Sway

--- modules/profiles/ivan770/sway/sway.nix
+++ modules/profiles/ivan770/sway/sway.nix
@@ -16,6 +16,7 @@ in {
       --no-generic
   '';
   bars = [];
+
   input."type:keyboard" = {
     xkb_layout = "us,ru,ua";
     xkb_options = "grp:lalt_lshift_toggle";
@@ -25,12 +26,16 @@ in {
     natural_scroll = "enabled";
     scroll_method = "two_finger";
   };
+
   output."*".bg = "${../backgrounds/mountain.png} fill";
   seat = {
     "*" = {
       xcursor_theme = "${config.gtk.cursorTheme.name} ${builtins.toString config.gtk.cursorTheme.size}";
     };
   };
+
+  gaps.smartBorders = "on";
+
   defaultWorkspace = "workspace number 1";
   up = "w";
   left = "a";

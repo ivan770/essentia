37ff0cf09e5bc7b28990070f4ae2763520dcbd8c
ivan770 <ivan@ivan770.me>
2022-11-01 14:12:42 +0200

    Prepare for wolfram tmpfs migration

--- modules/users/ivan770/home-impermanence.nix
+++ modules/users/ivan770/home-impermanence.nix
@@ -10,26 +10,42 @@
         directory = ".gnupg";
         mode = "0700";
       }
+      ".java"
       ".julia"
+      ".lunarclient"
+      ".minecraft"
       ".mozilla"
       {
         directory = ".pki";
         mode = "0700";
       }
+      ".steam"
 
       # .config/*
       {
         directory = ".config/dconf";
         mode = "0700";
       }
+      ".config/discord"
+      ".config/lunarclient"
       ".config/VSCodium"
 
       # .local/*
+      {
+        directory = ".local/share/Cisco Packet Tracer";
+        mode = "0700";
+      }
       ".local/share/direnv"
+      ".local/share/jupyter"
+      {
+        directory = ".local/share/Steam";
+        mode = "0700";
+      }
       {
         directory = ".local/share/TelegramDesktop";
         mode = "0700";
       }
+      ".local/share/vulkan"
       {
         directory = ".local/state/wireplumber";
         mode = "0700";
@@ -41,6 +57,8 @@
       "Downloads"
       "Music"
       "Pictures"
+      # Cisco Packet Tracer data directory
+      "pt"
       "Public"
       "Templates"
       "Videos"
@@ -52,6 +70,7 @@
         file = ".ssh/known_hosts";
         parentDirectory = {mode = "0700";};
       }
+      ".packettracer"
     ];
   };
 }

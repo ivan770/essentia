0abb93bfa9ce1dda79ed41e6349ead1537dcfce8
ivan770 <ivan@ivan770.me>
2022-11-20 16:20:16 +0200

    Trust loopback connections in PostgreSQL

--- modules/desktop/postgresql.nix
+++ modules/desktop/postgresql.nix
@@ -7,6 +7,11 @@
     enable = true;
     package = pkgs.postgresql_15;
     enableTCPIP = true;
+    # Trust loopback TCP/IP connections
+    # Derived from https://www.postgresql.org/docs/current/auth-pg-hba-conf.html
+    authentication = ''
+      host all all 127.0.0.1/32 trust
+    '';
   };
 
   # Explicitly disable auto-start since not every desktop session requires PostgreSQL.

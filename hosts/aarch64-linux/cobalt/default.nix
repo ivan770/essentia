d4e27b752eb377ddb078317964f6a23b453236de
ivan770 <ivan@ivan770.me>
2022-11-21 15:39:22 +0200

    Activate Hedgedoc service

--- hosts/aarch64-linux/cobalt/default.nix
+++ hosts/aarch64-linux/cobalt/default.nix
@@ -10,7 +10,7 @@
       inherit (nixosModules.users.ivan770) user;
       inherit (nixosModules.common) home-manager;
       inherit (nixosModules.hardware) firmware networking systemd-boot;
-      inherit (nixosModules.server) code-server generic nginx;
+      inherit (nixosModules.server) code-server generic hedgedoc nginx;
     }
     ++ [
       (modulesPath + "/profiles/qemu-guest.nix")
@@ -18,6 +18,14 @@
 
   essentia = {
     firmware.cpu.vendor = null;
+    containers.activatedConfigurations.hedgedoc = {
+      bindMounts.data = "/var/lib/hedgedoc-data";
+      network = {
+        localAddress = "192.168.100.1";
+        localAddress = "192.168.101.1";
+      };
+      specialArgs.domain = "docs.elusive.space";
+    };
     home-manager.profiles.ivan770 = "remote-code";
     networking = {
       dns.preset = "server";
@@ -25,6 +33,7 @@
     };
     nginx.activatedUpstreams = {
       "remote.elusive.space" = "code-server";
+      "docs.elusive.space" = "hedgedoc-main";
     };
     secrets.ssl = true;
     systemd-boot = {
